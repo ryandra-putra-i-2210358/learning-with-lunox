1e2677868d4b87869087cb5f6b6d48b637af3527
Ryandra Putra <andra180103@gmail.com>
2026-03-01 15:55:30 +0700

    Belajat Bikin Web

---
+++ flashbackphp/cakupan/latihan1/index.php
@@ -0,0 +1,81 @@
+<?php 
+
+// for($i = 0; $i < 3; $i++){
+//     echo "Helow";
+// }
+
+// $angka = 4;
+// if($angka == 4){
+//     echo "Hwlo";
+// }
+
+// function cekNumber ($angka1, $angka2){
+//     if($angka1 <= 0 && $angka2 <= 0){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+
+// }
+// function conditionNumber ($items){
+//     // $angka = $items;
+//     $data = [];
+//     if(cekNumber(2, 3) === 0){
+//         for($i = 0; $i <= $items; $i++){
+//             echo "Hai<br>";
+//             $data[] = $i;
+//         }
+//     }else {
+//         echo "Angka Tidak Valid";
+//     }
+//     // return $angka;
+//     return $data;
+// }
+// $angka = conditionNumber(3);
+// // var_dump($angka);
+
+// foreach($angka as $num => $index){
+//     echo "ini hai angka ke " . $index  . "<br>";
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../cakupan/latihan1/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <img src="../../cakupan/latihan1/images/wimsikal.jpeg" alt="">
+
+        <div class="navlink">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Service</a>
+        </div>
+
+
+
+
+    </nav>
+    <div class="container">
+        <div class="dua">
+            <div class="item1"></div>
+            <div class="item2"></div>
+        </div>
+        <div class="satu">
+            <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora facilis sunt vitae officia incidunt suscipit molestiae, voluptatem assumenda deleniti ea eveniet adipisci consectetur natus! Odio iusto unde ipsa veniam! Quia?</p> -->
+        </div>
+    </div>
+
+    <footer>Copy Rigt Blabla bala 2026</footer>
+
+    <script src="../../cakupan/latihan1/script.js"></script>
+    
+</body>
+</html>
