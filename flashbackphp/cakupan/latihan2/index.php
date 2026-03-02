d7fd61ebc25b996a0db827bf4b112b95f970ae07
Ryandra Putra <andra180103@gmail.com>
2026-03-02 16:38:58 +0700

    Latihan Flex Box

---
+++ flashbackphp/cakupan/latihan2/index.php
@@ -0,0 +1,136 @@
+<?php 
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../cakupan/latihan2/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- latihan 1 - Columns -->
+    <div class="container-satu">
+        <div class="kolom-utama">
+            <h2>Judul Artikel</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non animi esse impedit delectus eveniet libero saepe id, fugit commodi dolore magnam iste velit eum ullam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sed. Temporibus tenetur atque sed obcaecati voluptates. Cupiditate praesentium officia quae sunt, et quas neque, tenetur qui recusandae labore incidunt ratione ex facere error rerum vitae? Reiciendis est consequatur consequuntur tempore.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis blanditiis dicta consequatur sint rerum enim, aspernatur expedita, nisi repellendus iure dolore beatae, ad delectus. Corporis labore quibusdam perferendis quae aut.</p>
+        </div>
+        <div class="sidebar-satu">
+            <h3>Sidebar Satu</h3>
+            <ul>
+                <li><a href="">Link 1</a></li>
+                <li><a href="">Link 2</a></li>
+                <li><a href="">Link 3</a></li>
+                <li><a href="">Link 4</a></li>
+                <li><a href="">Link 5</a></li>
+            </ul>
+            
+        </div>
+        <div class="sidebar-dua">
+            <h3>SideBar 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea illo quas aspernatur esse soluta vel omnis, animi ipsum vero odit at incidunt nostrum. Magni eos dolores consequuntur, placeat sit ut?</p>
+        </div>
+    </div>
+
+
+    <!-- Latihan 2 Service/ Feature -->
+     <div class="container-dua">
+        <div class="layanan-satu">
+            <h4>Layanan 1</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti!</p>
+        </div>
+        <div class="layanan-dua">
+            <h4>Layanan 2</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis repellendus similique delectus, itaque omnis odio.</p>
+        </div>
+        <div class="layanan-tiga">
+            <h4>Layanan 3</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus nulla aut tempora quis voluptate aperiam amet ducimus modi.</p>
+        </div>
+     </div>
+
+
+     <!-- Akhir Latihan 2 -->
+
+     <!-- Latihan 3 -->
+      <div class="container-tiga">
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/1.jpg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/2.jpeg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/3.jpg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/3.jpg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/2.jpeg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/1.jpg">
+        </div>
+        <div class="thumnail">
+            <img src="../../cakupan/latihan2/images/1.jpg">
+        </div>
+      </div>
+
+      <!-- Akhir Latihan 3 -->
+
+
+      <!-- Latihan 4 -->
+
+      <div class="container-empat">
+        <div class="card">
+            <div class="header"></div>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem veritatis labore debitis nobis perspiciatis fugiat.</p>
+            </div>
+            <button>Buy Now</button>
+        </div>
+        <div class="card besar">
+            <div class="header"></div>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem veritatis labore debitis nobis perspiciatis fugiat.</p>
+            </div>
+            <button>Buy Now</button>
+        </div>
+        <div class="card">
+            <div class="header"></div>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem veritatis labore debitis nobis perspiciatis fugiat.</p>
+            </div>
+            <button>Buy Now</button>
+        </div>
+      </div>
+
+
+
+      <!-- AKhir latihan 4 -->
+
+
+      <!-- Latihan 5 centering -->
+
+      <div class="container-lima">
+        <div class="kotak"><span>Heloo World dssdsdsds</span></div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
