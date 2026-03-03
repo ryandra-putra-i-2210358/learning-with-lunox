df0e2ff0fea596af5e4ffcaa8ed8261d73054ffc
Ryandra Putra <andra180103@gmail.com>
2026-03-03 17:05:49 +0700

    Fitur NAV FLEX

---
+++ flashbackphp/cakupan/latihan3/index.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../cakupan/latihan3/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Pacifico&display=swap" rel="stylesheet">
+    <title>My Navbar</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h4>Ryanox</h4>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Product</a></li>
+        </ul>
+    </nav>
+
+
+    <script src="../../cakupan/latihan3/script.js"></script>
+</body>
+</html>
