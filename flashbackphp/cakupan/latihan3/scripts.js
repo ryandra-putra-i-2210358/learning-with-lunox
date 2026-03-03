b2485983491a3bb05121be9123901792ebbd8dd0
Ryandra Putra <andra180103@gmail.com>
2026-03-03 23:45:20 +0700

    Fitur Nav Done

--- flashbackphp/cakupan/latihan3/scripts.js
+++ flashbackphp/cakupan/latihan3/scripts.js
@@ -1,3 +1,6 @@
 
 const menuToggle = document.querySelector('.menu-toggle input');
 const navUl = document.querySelector('nav ul')
+menuToggle.addEventListener('click', () => {
+    navUl.classList.toggle('slide');
+})
