7216d4e3e6a0f0ebab67c5cd91bf0ef261bac388
Ryandra Putra <andra180103@gmail.com>
2026-03-03 23:29:19 +0700

    Hamburger Menu

--- flashbackphp/cakupan/latihan3/index.php
+++ flashbackphp/cakupan/latihan3/index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../cakupan/latihan3/style.css">
+    <link rel="stylesheet" href="../../cakupan/latihan3/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Pacifico&display=swap" rel="stylesheet">
@@ -19,7 +19,7 @@
 <body>
     <nav>
         <div class="logo">
-            <h4>Ryanox</h4>
+            <h4>Ryandra Lunox</h4>
         </div>
         <ul>
             <li><a href="#">Home</a></li>
@@ -28,6 +28,13 @@
             <li><a href="#">Gallery</a></li>
             <li><a href="#">Product</a></li>
         </ul>
+
+        <div class="menu-toggle">
+            <input type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </nav>
 
 
